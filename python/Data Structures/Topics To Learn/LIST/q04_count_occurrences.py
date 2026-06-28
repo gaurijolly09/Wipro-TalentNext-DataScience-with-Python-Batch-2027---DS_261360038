c1ade4be70d2f57867ca0e977c721d0d4bbef67f
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:28:36 +0530

    Add script to count occurrences of a color in a list

---
+++ python/Data Structures/Topics To Learn/LIST/q04_count_occurrences.py
@@ -0,0 +1,6 @@
+collection_of_colors = ["Red", "Blue", "Green", "Red", "Yellow", "Red"]
+
+color_to_look_for = "Red"
+how_many_times_it_appears = collection_of_colors.count(color_to_look_for)
+
+print(f"The color {color_to_look_for} showed up {how_many_times_it_appears} times in the list.")
