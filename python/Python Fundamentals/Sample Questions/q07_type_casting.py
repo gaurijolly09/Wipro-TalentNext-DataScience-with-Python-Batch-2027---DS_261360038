6be0a51fb25aa3a33d13abbba588f5837a8d35f7
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:29:51 +0530

    Create q07_type_casting.py

---
+++ python/Python Fundamentals/Sample Questions/q07_type_casting.py
@@ -0,0 +1,4 @@
+age_as_string = "25"
+age_as_integer = int(age_as_string)
+print("Type casting is converting a value from one data type to another.")
+print(f"We converted the string '{age_as_string}' into an integer. Now its type is: {type(age_as_integer)}")
