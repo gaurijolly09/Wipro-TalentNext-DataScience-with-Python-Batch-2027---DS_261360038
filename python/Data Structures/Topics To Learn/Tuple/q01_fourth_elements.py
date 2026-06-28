07d618a173da0f9291531d5d1c7e221ebcdb1c85
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:43:50 +0530

    Add script to print fourth elements of a tuple

---
+++ python/Data Structures/Topics To Learn/Tuple/q01_fourth_elements.py
@@ -0,0 +1,7 @@
+my_random_items = ("apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew")
+
+fourth_item_from_start = my_random_items[3]
+fourth_item_from_end = my_random_items[-4]
+
+print("The 4th item counting from the beginning is:", fourth_item_from_start)
+print("The 4th item counting backwards from the end is:", fourth_item_from_end)
