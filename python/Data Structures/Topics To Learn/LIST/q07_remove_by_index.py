b1bf33bad3e386d2cf27f1d17675dc4cb8634954
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:31:10 +0530

    Add script to remove item by index from list

---
+++ python/Data Structures/Topics To Learn/LIST/q07_remove_by_index.py
@@ -0,0 +1,7 @@
+things_on_my_desk = ["Pen", "Notebook", "Coffee Mug", "Laptop"]
+print("Things currently on desk:", things_on_my_desk)
+
+position_to_clear = 2
+things_on_my_desk.pop(position_to_clear)
+
+print("Things on desk after removing the item at that position:", things_on_my_desk)
