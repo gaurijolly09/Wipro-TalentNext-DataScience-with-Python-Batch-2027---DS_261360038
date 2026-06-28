d38a65a6cad6ae0e1808035be57d97017e897b7c
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:27:56 +0530

    Add script to reverse a list of letters

---
+++ python/Data Structures/Topics To Learn/LIST/q03_reverse_list.py
@@ -0,0 +1,6 @@
+a_bunch_of_letters = ["A", "B", "C", "D", "E"]
+print("Original arrangement:", a_bunch_of_letters)
+
+a_bunch_of_letters.reverse()
+
+print("Flipped arrangement:", a_bunch_of_letters)
