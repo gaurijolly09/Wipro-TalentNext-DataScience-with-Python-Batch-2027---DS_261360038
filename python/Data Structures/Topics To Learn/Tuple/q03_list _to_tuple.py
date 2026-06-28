35f9d5272006e7b0875da55e3827fc534ced5c9f
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:45:15 +0530

    Add script to convert list to tuple

---
+++ python/Data Structures/Topics To Learn/Tuple/q03_list _to_tuple.py
@@ -0,0 +1,8 @@
+a_bunch_of_numbers = [1, 2, 3, 4, 5]
+print("Here is our initial list:", a_bunch_of_numbers)
+print("Its type is:", type(a_bunch_of_numbers))
+
+locked_numbers_tuple = tuple(a_bunch_of_numbers)
+
+print("\nHere is our converted tuple:", locked_numbers_tuple)
+print("Its new type is:", type(locked_numbers_tuple))
