be4e28d53d14cbd5a00e079a8fcd753c45003fc2
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:28:30 +0530

    Create q05_object_location.py

---
+++ python/Python Fundamentals/Sample Questions/q05_object_location.py
@@ -0,0 +1,4 @@
+sample_text = "Learning Python"
+memory_location = id(sample_text)
+print(f"The id() function returns the location of an object.")
+print(f"For example, the object '{sample_text}' is located at memory address: {memory_location}")
