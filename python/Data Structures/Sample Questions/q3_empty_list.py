960864eee396605e923db5187ba4d472a1c714bc
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:12:24 +0530

    Add q3_empty_list.py to demonstrate list clearing

---
+++ python/Data Structures/Sample Questions/q3_empty_list.py
@@ -0,0 +1,6 @@
+shopping_cart = ["Shirt", "Pants", "Hat"]
+print("Items before clearing:", shopping_cart)
+
+shopping_cart.clear()
+
+print("The clear() function empties the list entirely:", shopping_cart)
