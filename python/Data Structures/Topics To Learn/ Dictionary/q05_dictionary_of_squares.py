47a55afc99c4273f6a94dd01e14b997bf15f30f8
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:24:41 +0530

    Add dictionary of squares from 1 to 15

---
+++ python/Data Structures/Topics To Learn/ Dictionary/q05_dictionary_of_squares.py
@@ -0,0 +1,7 @@
+squares_collection = {}
+
+for current_number in range(1, 16):
+    squares_collection[current_number] = current_number * current_number
+
+print("The final dictionary filled with squares:")
+print(squares_collection)
