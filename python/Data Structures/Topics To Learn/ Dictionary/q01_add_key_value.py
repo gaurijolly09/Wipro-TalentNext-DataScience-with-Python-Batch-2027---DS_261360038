7d4f6b714b618015a325ed1d5d2cae00af472fe2
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:21:22 +0530

    Add key-value pair to starting dictionary

---
+++ python/Data Structures/Topics To Learn/ Dictionary/q01_add_key_value.py
@@ -0,0 +1,9 @@
+my_starting_dictionary = {0: 10, 1: 20}
+print("Dictionary before adding:", my_starting_dictionary)
+
+new_key_to_add = 2
+new_value_to_add = 30
+
+my_starting_dictionary[new_key_to_add] = new_value_to_add
+
+print("Dictionary after adding:", my_starting_dictionary)
