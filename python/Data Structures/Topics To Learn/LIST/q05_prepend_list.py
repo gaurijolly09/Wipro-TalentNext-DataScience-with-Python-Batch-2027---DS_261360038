d5a4901b349492a129fa17eda35162f979f2a285
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:29:15 +0530

    Add script to prepend one list to another

---
+++ python/Data Structures/Topics To Learn/LIST/q05_prepend_list.py
@@ -0,0 +1,9 @@
+first_group = ["Apple", "Banana"]
+second_group = ["Cherry", "Date"]
+
+print("First group:", first_group)
+print("Second group:", second_group)
+
+second_group = first_group + second_group
+
+print("Second group after putting the first group in front of it:", second_group)
