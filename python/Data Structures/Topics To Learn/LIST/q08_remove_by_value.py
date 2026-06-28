211227024cade64547ff1a3befb7d2d5b5127e1d
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:32:34 +0530

    Add script to remove a chore by value from list

---
+++ python/Data Structures/Topics To Learn/LIST/q08_remove_by_value.py
@@ -0,0 +1,7 @@
+daily_chores = ["Wash dishes", "Sweep floor", "Wash dishes", "Take out trash"]
+print("Chores for today:", daily_chores)
+
+chore_that_is_done = "Wash dishes"
+daily_chores.remove(chore_that_is_done)
+
+print("Chores left after doing that specific one the first time:", daily_chores)
