04fec20a7c2da46521e1ae6e3f5decbe2443d5f3
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:29:11 +0530

    Add integer division example in q06_interger_division.py

---
+++ python/Python Fundamentals/Sample Questions/q06_interger_division.py
@@ -0,0 +1,5 @@
+total_apples = 10
+people = 3
+apples_per_person = total_apples // people
+print("The double slash (//) operator is used for integer division.")
+print(f"If we divide {total_apples} apples among {people} people, each gets {apples_per_person} apples.")
