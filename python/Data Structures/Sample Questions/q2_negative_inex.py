1adefd7d77166548dd3b365f42e7708e9f112d25
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:11:28 +0530

    Add example of negative indexing in Python

---
+++ python/Data Structures/Sample Questions/q2_negative_inex.py
@@ -0,0 +1,4 @@
+my_favorite_numbers = [10, 20, 30, 40]
+print("Yes, we can absolutely access items with a negative index!")
+last_item = my_favorite_numbers[-1]
+print("The last item accessed using -1 is:", last_item)
