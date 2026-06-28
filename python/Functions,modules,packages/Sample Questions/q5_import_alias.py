4850b9c6bdf7bd3b2261e33db1f7cc302b5f66fc
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:05:09 +0530

    Add example of module import with alias
    Demonstrate importing a module with an alias and calculating the square root of 16.

---
+++ python/Functions,modules,packages/Sample Questions/q5_import_alias.py
@@ -0,0 +1,6 @@
+import math as mathematics_module
+
+the_square_root = mathematics_module.sqrt(16)
+print("You use the 'as' keyword to give it an alias.")
+print("Example: import math as mathematics_module")
+print("Square root of 16 using the alias:", the_square_root)
