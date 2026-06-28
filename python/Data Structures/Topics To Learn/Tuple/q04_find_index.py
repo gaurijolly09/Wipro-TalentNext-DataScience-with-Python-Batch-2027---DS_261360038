5785f77dce2e1ecb9afa3fe06c9c81310a54098b
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:45:52 +0530

    Add function to find index of a color in tuple

---
+++ python/Data Structures/Topics To Learn/Tuple/q04_find_index.py
@@ -0,0 +1,6 @@
+rainbow_colors = ("red", "orange", "yellow", "green", "blue", "indigo", "violet")
+
+color_we_want_to_find = "green"
+exact_position = rainbow_colors.index(color_we_want_to_find)
+
+print(f"The color {color_we_want_to_find} is sitting at index {exact_position}.")
