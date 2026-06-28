603ac525d2aa36e6e09e9744ff5712c1f61573a5
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:14:28 +0530

    Add slicing example in q5_slicing.py

---
+++ python/Data Structures/Sample Questions/q5_slicing.py
@@ -0,0 +1,6 @@
+a_long_word = "Supercalifragilisticexpialidocious"
+a_small_slice = a_long_word[0:5]
+
+print("Slicing is grabbing a specific chunk of a sequence.")
+print("The original word is:", a_long_word)
+print("The sliced chunk is:", a_small_slice)
