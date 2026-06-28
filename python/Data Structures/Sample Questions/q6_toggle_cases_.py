4ed3a957d52786fa2f3ecbf200a526c93cd8345e
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:16:39 +0530

    Add toggle cases example using swapcase()

---
+++ python/Data Structures/Sample Questions/q6_toggle_cases_.py
@@ -0,0 +1,6 @@
+mixed_up_text = "hELLO wORLD"
+fixed_text = mixed_up_text.swapcase()
+
+print("The swapcase() function toggles the cases perfectly.")
+print("Original:", mixed_up_text)
+print("Toggled:", fixed_text)
