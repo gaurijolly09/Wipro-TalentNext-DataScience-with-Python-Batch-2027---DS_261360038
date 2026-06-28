0d9e05d2c57cdc51645dd1b63fd1ca6d67a1a8e6
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:31:11 +0530

    Add example usage of range() function

---
+++ python/Python Fundamentals/Sample Questions/q09_integer_sequence.py
@@ -0,0 +1,5 @@
+sequence_function_name = "range()"
+print(f"The {sequence_function_name} built-in function returns a sequence of integers.")
+print("Example output of range(5):")
+for number in range(5):
+    print(number)
