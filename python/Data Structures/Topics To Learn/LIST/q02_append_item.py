f18267ceb3ce2294a7199077196bed95b89fc2fa
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:27:14 +0530

    Add script to append item to grocery list

---
+++ python/Data Structures/Topics To Learn/LIST/q02_append_item.py
@@ -0,0 +1,7 @@
+grocery_items = ["Apples", "Bread", "Milk"]
+print("Initial groceries:", grocery_items)
+
+new_item_to_buy = "Eggs"
+grocery_items.append(new_item_to_buy)
+
+print("Updated groceries after adding something new:", grocery_items)
