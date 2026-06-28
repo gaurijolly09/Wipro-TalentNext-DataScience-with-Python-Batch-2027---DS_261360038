5fd08d2e706e1fe3a62ddfc062aa92833b6c3212
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:31:54 +0530

    Add example of else with for loop in Python

---
+++ python/Python Fundamentals/Sample Questions/q10_else_with_for.py
@@ -0,0 +1,7 @@
+print("Can we use else with for? Yes, we can.")
+print("Example:")
+
+for number in range(3):
+    print(f"Checking number {number}")
+else:
+    print("This else block executed because the for loop finished without breaking.")
