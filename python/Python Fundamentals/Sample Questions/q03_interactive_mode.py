7f55bb2445f69dfa77a0090838d94d91cfed502b
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:27:07 +0530

    Add interactive mode instructions to q03_interactive_mode.py

---
+++ python/Python Fundamentals/Sample Questions/q03_interactive_mode.py
@@ -0,0 +1,4 @@
+interactive_commands = ["python", "python3", "py"]
+print("To enable the interactive terminal mode, you can type one of the following commands:")
+for command in interactive_commands:
+    print("- " + command)
