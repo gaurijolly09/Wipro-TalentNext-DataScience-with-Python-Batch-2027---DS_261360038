4a7ade901289a47340f603ac184497859c12a464
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:07:28 +0530

    Create q6_list_builtins.py

---
+++ python/Functions,modules,packages/Sample Questions/q6_list_builtins.py
@@ -0,0 +1,5 @@
+import sys
+
+print("You can use 'help(\"modules\")' in the interactive prompt, or check 'sys.builtin_module_names'!")
+print("Here are just a few built-in module names from sys:")
+print(sys.builtin_module_names[:5])
