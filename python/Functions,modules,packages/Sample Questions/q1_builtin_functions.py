d94f2cf52f26e57f6e8d60f0b81040cf09152abc
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:02:29 +0530

    Add sample built-in functions in Python

---
+++ python/Functions,modules,packages/Sample Questions/q1_builtin_functions.py
@@ -0,0 +1,7 @@
+the_first_built_in = "print()"
+the_second_built_in = "len()"
+the_third_built_in = "max()"
+the_fourth_built_in = "type()"
+
+print("Here are a few very handy built-in functions in Python:")
+print(the_first_built_in, the_second_built_in, the_third_built_in, the_fourth_built_in)
