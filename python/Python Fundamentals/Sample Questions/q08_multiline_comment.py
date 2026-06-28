e61686ae295f3dc85c4d84f528262a0664eba03a
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 04:30:30 +0530

    Add example of multi-line comment in Python

---
+++ python/Python Fundamentals/Sample Questions/q08_multiline_comment.py
@@ -0,0 +1,7 @@
+multi_line_comment_explanation = """
+You can do a multi-line comment using triple quotes 
+like this string, or by placing a hash symbol 
+at the beginning of every single line.
+"""
+print("How to do a multi-line comment:")
+print(multi_line_comment_explanation)
