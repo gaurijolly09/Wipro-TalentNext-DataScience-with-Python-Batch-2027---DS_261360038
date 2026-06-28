ddd5dd0b53d5de9f66fc4c1ef5ae2bb5096f3f5b
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:18:57 +0530

    Add example of set ignoring duplicates
    Demonstrate set behavior with duplicate elements.

---
+++ python/Data Structures/Sample Questions/q7_set_duplicates.py
@@ -0,0 +1,4 @@
+a_collection_of_numbers = {1, 2, 2, 2, 3, 4, 4}
+
+print("No, a set completely ignores duplicate elements.")
+print("Even though we tried adding multiple 2s and 4s, the set only contains:", a_collection_of_numbers)
