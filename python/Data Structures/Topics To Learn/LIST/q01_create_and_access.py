3a4a09a923b9fe1028f1418c78bcd0ff71e1a158
gaurijolly09 <gaurijolly09@gmail.com>
2026-06-29 03:26:37 +0530

    Add script to create and access a list of numbers

---
+++ python/Data Structures/Topics To Learn/LIST/q01_create_and_access.py
@@ -0,0 +1,9 @@
+my_favorite_numbers = [10, 25, 42, 73, 99]
+
+print("The whole collection of numbers is:", my_favorite_numbers)
+
+print("The very first number is:", my_favorite_numbers[0])
+print("The second number is:", my_favorite_numbers[1])
+print("The third number is:", my_favorite_numbers[2])
+print("The fourth number is:", my_favorite_numbers[3])
+print("The fifth and final number is:", my_favorite_numbers[4])
